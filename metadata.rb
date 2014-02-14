090c86c2aa99ab2ecdca1121c4879668db0e414e
Peter Crossley <peter.crossley@webtrends.com>
2014-02-14 14:15:24 -0800

    fixed the README path

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Webtrends, Inc."
 maintainer_email "Peter Crossley <peter.crossley@webtrends.com>"
 license          "All rights reserved"
 description      "Installs/Configures WinRM"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 supports         "windows"
 depends          "windows"
