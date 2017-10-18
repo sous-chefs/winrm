0dc3af9d0c646b3883eed944043ea8f06c72a6ba
Jamie Zaharopoulos <JZaharopoulos@vigilantglobal.com>
2017-10-18 13:58:16 -0400

    merged with code from powershell cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-default['winrm'] = {}
-default['winrm']['thumbprint'] = nil
 default['winrm']['https'] = true
+default['winrm']['thumbprint'] = nil # mandatory for https transport
 default['winrm']['http'] = true
 default['winrm']['BasicAuth'] = true
 default['winrm']['MaxMemoryPerShellMB'] = 1024
 default['winrm']['AllowUnencrypted'] = true
 default['winrm']['TrustedHosts'] = '*'
 default['winrm']['Everyone_Group'] = 'Everyone'
+default['winrm']['hostname'] = node['fqdn']
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,103 +16,126 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+if platform_family?('windows')
 
-powershell_script 'search-for-thumbprint' do
-  cwd Chef::Config[:file_cache_path]
-  code "Get-childItem cert:\\LocalMachine\\Root\\ | Select-String -pattern #{node['fqdn']} | Select-Object -first 1 -ExpandProperty line | % { $_.SubString($_.IndexOf('[Thumbprint]')+ '[Thumbprint]'.Length).Trim()} | Out-File -Encoding \"ASCII\" winrm.thumbprint"
-  only_if { node['winrm']['thumbprint'].nil? && node['winrm']['https'] }
-  notifies :create, 'file[fix-perms-thumbprint]', :immediately
-end
-
-file 'fix-perms-thumbprint' do
-  action :nothing
-  path "#{Chef::Config[:file_cache_path]}\\winrm.thumbprint"
-  backup false
-  rights :read, node['winrm']['Everyone_Group']
-end
-
-ruby_block 'read-winrm-thumbprint' do
-  block do
-    f = File.open("#{Chef::Config[:file_cache_path]}\\winrm.thumbprint", 'r')
-    val = f.read.strip
-    f.close
-    node.default['winrm']['thumbprint'] = ((val.empty? || val.nil?) ? nil : val)
+  powershell_script 'search-for-thumbprint' do
+    cwd Chef::Config[:file_cache_path]
+    code "Get-childItem cert:\\LocalMachine\\Root\\ | Select-String -pattern #{node['winrm']['hostname']} | Select-Object -first 1 -ExpandProperty line | % { $_.SubString($_.IndexOf('[Thumbprint]')+ '[Thumbprint]'.Length).Trim()} | Out-File -Encoding \"ASCII\" winrm.thumbprint"
+    only_if { node['winrm']['thumbprint'].nil? && node['winrm']['https'] }
+    # notifies :create, 'file[fix-perms-thumbprint]', :immediately
   end
-  only_if { !File.zero?("#{Chef::Config[:file_cache_path]}\\winrm.thumbprint") && node['winrm']['thumbprint'].nil? && node['winrm']['https'] }
-end
 
-cookbook_file "#{Chef::Config[:file_cache_path]}\\selfssl.exe" do
-  source 'selfssl.exe'
-  only_if { node['winrm']['https'] && node['winrm']['thumbprint'].nil? }
-end
+  # file 'fix-perms-thumbprint' do
+  # action :nothing
+  # path "#{Chef::Config[:file_cache_path]}\\winrm.thumbprint"
+  # backup false
+  # rights :read, node['winrm']['Everyone_Group']
+  # end
 
-execute 'create-certificate' do
-  command "#{Chef::Config[:file_cache_path]}\\selfssl.exe /T /N:cn=#{node['fqdn']} /V:3650 /Q"
-  only_if { node['winrm']['https'] && node['winrm']['thumbprint'].nil? }
-  notifies :run, 'powershell_script[search-for-thumbprint]', :immediately
-  notifies :create, 'ruby_block[read-winrm-thumbprint]', :immediately
-  notifies :delete, 'file[cleanup-thumbprint]'
-end
+  ruby_block 'read-winrm-thumbprint' do
+    block do
+      f = File.open("#{Chef::Config[:file_cache_path]}\\winrm.thumbprint", 'r')
+      val = f.read.strip
+      f.close
+      node.default['winrm']['thumbprint'] = ((val.empty? || val.nil?) ? nil : val)
+    end
+    only_if { !File.zero?("#{Chef::Config[:file_cache_path]}\\winrm.thumbprint") && node['winrm']['thumbprint'].nil? && node['winrm']['https'] }
+  end
 
-powershell_script 'winrm-quickconfig' do
-  code 'winrm quickconfig -quiet'
-  only_if { node['winrm']['http'] }
-end
+  cookbook_file "#{Chef::Config[:file_cache_path]}\\selfssl.exe" do
+    source 'selfssl.exe'
+    only_if { node['winrm']['https'] && node['winrm']['thumbprint'].nil? }
+  end
 
-powershell_script 'winrm-create-https-listener' do
-  code "winrm create 'winrm/config/Listener?Address=*+Transport=HTTPS' '@{Hostname=\"#{node['fqdn']}\"; CertificateThumbprint=\"#{node['winrm']['thumbprint']}\"}'"
-  only_if { node['winrm']['https'] && !File.zero?("#{Chef::Config[:file_cache_path]}\\winrm.thumbprint") }
-end
+  execute 'create-certificate' do
+    command "#{Chef::Config[:file_cache_path]}\\selfssl.exe /T /N:cn=#{node['winrm']['hostname']} /V:3650 /Q"
+    only_if { node['winrm']['https'] && node['winrm']['thumbprint'].nil? }
+    notifies :run, 'powershell_script[search-for-thumbprint]', :immediately
+    notifies :create, 'ruby_block[read-winrm-thumbprint]', :immediately
+    notifies :delete, 'file[cleanup-thumbprint]', :immediately
+  end
 
-file 'cleanup-thumbprint' do
-  path "#{Chef::Config[:file_cache_path]}\\winrm.thumbprint"
-  action :delete
-  backup false
-end
+  file 'cleanup-thumbprint' do
+    path "#{Chef::Config[:file_cache_path]}\\winrm.thumbprint"
+    action :delete
+    backup false
+  end
 
-powershell_script 'winrm-auth' do
-  code "winrm set winrm/config/service/Auth '@{Basic=\"#{node['winrm']['BasicAuth']}\"}'"
-end
+  # Configure winrm
+  # use attributes to add other configuration
+  powershell_script 'enable winrm' do
+    code <<-EOH
+      winrm quickconfig -q
+    EOH
+  end
 
-powershell_script 'winrm-unencrypted' do
-  code "winrm set winrm/config/service '@{AllowUnencrypted=\"#{node['winrm']['AllowUnencrypted']}\"}'"
-end
+  # check if https listener already exists
+  winrm_cmd = 'powershell.exe winrm enumerate winrm/config/listener'
+  shell_out = Mixlib::ShellOut.new(winrm_cmd)
+  shell_out.run_command
 
-powershell_script 'winrm-winrs' do
-  code "winrm set winrm/config/winrs '@{MaxMemoryPerShellMB=\"#{node['winrm']['MaxMemoryPerShellMB']}\"}'"
-end
+  # Create HTTPS listener
+  if !shell_out.stdout.include?('Transport = HTTPS') && node['winrm']['https']
+    if node['winrm']['thumbprint'].nil? || node['winrm']['thumbprint'].empty?
+      Chef::Log.error('Please specify thumbprint in default attributes for enabling https transport.')
+    else
+      powershell_script 'winrm-create-https-listener' do
+        code "winrm create 'winrm/config/Listener?Address=*+Transport=HTTPS' '@{Hostname=\"#{node['winrm']['hostname']}\"; CertificateThumbprint=\"#{node['winrm']['thumbprint']}\"}'"
+      end
+    end
+  else
+    Chef::Log.warn('WinRM HTTPS listener is already configured. Please delete the existing https listener first to configure new one.')
+  end
 
-powershell_script 'winrm-winrs-trustedhosts' do
-  code "winrm set winrm/config/client '@{TrustedHosts=\"#{node['winrm']['TrustedHosts']}\"}'"
-end
+  # Create HTTP listener
+  if !shell_out.stdout.include?('Transport = HTTP') && node['winrm']['http']
+    powershell_script 'winrm-create-https-listener' do
+      code "winrm set winrm/config/Listener?Address=*+Transport=HTTP '@{Hostname=\"#{node['winrm']['hostname']}\"}'"
+    end
+  else
+    Chef::Log.warn('WinRM HTTP listener is already configured. Please delete the existing https listener first to configure new one.')
+  end
 
-powershell_script 'winrm-http-listener' do
-  code "winrm set winrm/config/Listener?Address=*+Transport=HTTP '@{Hostname=\"#{node['fqdn']}\"}'"
-  only_if { node['winrm']['http'] }
-end
+  # Configure extended options
+  powershell_script 'winrm-auth' do
+    code "winrm set winrm/config/service/Auth '@{Basic=\"#{node['winrm']['BasicAuth']}\"}'"
+  end
 
-powershell_script 'winrm-https-listener' do
-  code "winrm set 'winrm/config/Listener?Address=*+Transport=HTTPS' '@{Hostname=\"#{node['fqdn']}\"; CertificateThumbprint=\"#{node['winrm']['thumbprint']}\"}'"
-  only_if { node['winrm']['https'] && !node['winrm']['thumbprint'].nil? }
-end
+  powershell_script 'winrm-unencrypted' do
+    code "winrm set winrm/config/service '@{AllowUnencrypted=\"#{node['winrm']['AllowUnencrypted']}\"}'"
+  end
 
-# unlock port in firewall
-# this should leverage firewall_rule resource
-# once COOK-689 is completed
-firewall_rule_name = 'WINRM HTTP Static Port'
+  powershell_script 'winrm-winrs' do
+    code "winrm set winrm/config/winrs '@{MaxMemoryPerShellMB=\"#{node['winrm']['MaxMemoryPerShellMB']}\"}'"
+  end
 
-execute 'open-static-port-http' do
-  command "netsh advfirewall firewall add rule name=\"#{firewall_rule_name}\" dir=in action=allow protocol=TCP localport=5985"
-  returns [0, 1, 42] # *sigh* cmd.exe return codes are wonky
-  not_if { Winrm::Helper.firewall_rule_enabled?(firewall_rule_name) }
-  only_if { node['winrm']['http'] }
-end
+  powershell_script 'winrm-winrs-trustedhosts' do
+    code "winrm set winrm/config/client '@{TrustedHosts=\"#{node['winrm']['TrustedHosts']}\"}'"
+  end
 
-firewall_rule_name = 'WINRM HTTPS Static Port'
+  powershell_script 'winrm-http-listener' do
+    code "winrm set winrm/config/Listener?Address=*+Transport=HTTP '@{Hostname=\"#{node['fqdn']}\"}'"
+    only_if { node['winrm']['http'] }
+  end
 
-execute 'open-static-port-https' do
-  command "netsh advfirewall firewall add rule name=\"#{firewall_rule_name}\" dir=in action=allow protocol=TCP localport=5986"
-  returns [0, 1, 42] # *sigh* cmd.exe return codes are wonky
-  not_if { Winrm::Helper.firewall_rule_enabled?(firewall_rule_name) }
-  only_if { node['winrm']['https'] && !node['winrm']['thumbprint'].nil? }
+  # Allow port in firewall
+  firewall_rule_name = 'WINRM HTTP Static Port'
+
+  execute 'open-static-port-http' do
+    command "netsh advfirewall firewall add rule name=\"#{firewall_rule_name}\" dir=in action=allow protocol=TCP localport=5985"
+    returns [0, 1, 42] # *sigh* cmd.exe return codes are wonky
+    not_if { Winrm::Helper.firewall_rule_enabled?(firewall_rule_name) }
+    only_if { node['winrm']['http'] }
+  end
+
+  firewall_rule_name = 'WINRM HTTPS Static Port'
+
+  execute 'open-static-port-https' do
+    command "netsh advfirewall firewall add rule name=\"#{firewall_rule_name}\" dir=in action=allow protocol=TCP localport=5986"
+    returns [0, 1, 42] # *sigh* cmd.exe return codes are wonky
+    not_if { Winrm::Helper.firewall_rule_enabled?(firewall_rule_name) }
+    only_if { node['winrm']['https'] && !node['winrm']['thumbprint'].nil? }
+  end
+else
+  Chef::Log.warn('WinRM can only be enabled on the Windows platform.')
 end
